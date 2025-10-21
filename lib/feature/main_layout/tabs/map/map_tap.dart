b0676581eb31d5d96c4feb1f40f5f1c345ab0a43
G17e5 <geogamilrge23@gmail.com>
2025-10-21 10:40:31 +0300

    finshied maps tab and determine user location

--- lib/feature/main_layout/tabs/map/map_tap.dart
+++ lib/feature/main_layout/tabs/map/map_tap.dart
@@ -1,5 +1,7 @@
+import 'package:event_app/core/resource/colors_manager/colors_manager.dart';
 import 'package:event_app/providers/maps_tab_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 class MapTap extends StatelessWidget {
@@ -7,9 +9,34 @@ class MapTap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    MapsTabProvider mapsTabProvider = Provider.of<MapsTabProvider>(context);
-    return Center(child: Text(
-      mapsTabProvider.locationMessage
-    ));
+    MapsTabProvider provider = Provider.of<MapsTabProvider>(context);
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: GoogleMap(
+              initialCameraPosition: provider.cameraPosition,
+              onMapCreated: (controller) => {
+                provider.googleMapController = controller,
+              },
+              mapType: MapType.normal,
+              markers: provider.markers,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          provider.getLocation();
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+        foregroundColor: ColorsManager.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Icon(Icons.gps_fixed_outlined),
+      ),
+    );
   }
 }
--- lib/providers/maps_tab_provider.dart
+++ lib/providers/maps_tab_provider.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class MapsTabProvider extends ChangeNotifier {
   final Location location = Location();
-  String locationMessage = '';
+  late GoogleMapController googleMapController;
 
-  MapsTabProvider() {
-    getLocation();
-  }
+  CameraPosition cameraPosition = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Set<Marker> markers = {};
+
+  // MapsTabProvider() {
+  //   getLocation();
+  //   // setLocationListener();
+  // }
 
   Future<bool> _getLocationPermission() async {
     PermissionStatus permissionStatus;
@@ -31,36 +40,46 @@ class MapsTabProvider extends ChangeNotifier {
   Future<void> getLocation() async {
     bool permissionGranted = await _getLocationPermission();
     if (!permissionGranted) {
-      locationMessage = 'Permission denied';
-      notifyListeners();
       return;
     }
     bool serviceEnabled = await _checkLocationServices();
     if (!serviceEnabled) {
-      locationMessage = 'Location Permission disabled';
-      notifyListeners();
       return;
     }
-    locationMessage = 'Location service enabled and now are getting location';
-    notifyListeners();
+
     LocationData locationData = await location.getLocation();
-    locationMessage =
-        'Location: ${locationData.latitude} ,${locationData.longitude}';
+    changeLocationOnMap(locationData);
     notifyListeners();
   }
+
+  void changeLocationOnMap(LocationData locationData) {
+    cameraPosition = CameraPosition(
+      target: LatLng(locationData.latitude ?? 0, locationData.longitude ?? 0),
+      zoom: 14.4746,
+    );
+
+    markers.add(
+      Marker(
+        markerId: MarkerId("1"),
+        position: LatLng(
+          locationData.latitude ?? 0,
+          locationData.longitude ?? 0,
+        ),
+        infoWindow: InfoWindow(title: "Current Location"),
+      ),
+    );
+
+    googleMapController.animateCamera(
+      CameraUpdate.newCameraPosition(cameraPosition),
+    );
+    notifyListeners();
+  }
+
+  void setLocationListener() {
+    location.changeSettings(accuracy: LocationAccuracy.high, interval: 1000);
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      changeLocationOnMap(currentLocation);
+      notifyListeners();
+    });
+  }
 }
-
-//
-//
-//   bool serviceEnabled = await _location.serviceEnabled();
-//   if (!serviceEnabled) {
-//     serviceEnabled = await _location.requestService();
-//     if (!serviceEnabled) return;
-//   }
-//
-//   currentLocation = await _location.getLocation();
-//   notifyListeners(); // update UI when location changes
-
-//     if (permissionStatus != PermissionStatus.granted) {
-//       return false;
-//     }
